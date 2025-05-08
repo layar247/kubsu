5e38e513714f4343041dd791aa4c7bcdce0cf7fc
Владислав <vladcepurko@gmail.com>
2025-05-08 18:19:08 +0300

    Remove local admin directory from index

--- admin/bash/5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in /var/log/*.log; do
-    if [ -f "$file" ]; then
-        tail -n 1 "$file" >> logs.log
-    fi
-done
--- admin/bash/8.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ $(env | grep "^LC_" | cut -d'=' -f2 | uniq | wc -l) -gt 1 ]; then
-    exit 1
-fi
