e2ae6929493c2e555b2ae560b544475291b71cbe
Владислав <vladcepurko@gmail.com>
2025-05-09 21:05:04 +0300

    great

--- admin/bash/5.sh
+++ admin/bash/5.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash 
 
 for file in /var/log/*.log; do
     if [ -f "$file" ]; then
-        tail -n 1 "$file" >> logs.log
+        tail -n 1 "$file" 2>/dev/null >> logs.log
     fi
 done
