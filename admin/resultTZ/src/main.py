fa70aa5df25e712eef54bdb4ea4b599ff680d727
Владислав <vladcepurko@gmail.com>
2025-05-18 20:16:13 +0300

    read

--- admin/resultTZ/src/main.py
+++ admin/resultTZ/src/main.py
@@ -1,6 +1,5 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends, HTTPException
-from fastapi.middleware.cors import CORSMiddleware  # Добавил CORS
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from src import models, schemas
